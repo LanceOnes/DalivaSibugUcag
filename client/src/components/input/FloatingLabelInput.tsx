d1281e42ea1cb8d37f665fd3b28808ab9d1cfd50
LansOnes <johnleonida747@gmail.com>
2026-04-06 02:14:34 +0800

    adding FloatingLabelSelect and Tables

--- client/src/components/input/FloatingLabelInput.tsx
+++ client/src/components/input/FloatingLabelInput.tsx
@@ -55,9 +55,10 @@ const FloatingLabelInput: FC<FloatingLabelInputProps> = ({
                 />
                 <label
                     htmlFor={name}
-                    className={`${newLabelClassName ? newLabelClassName : `absolute text-sm text-body duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-left inset-start-2.5 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 
-                    peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 
-                    rtl:peer-focus:left-auto 
+                    className={`${newLabelClassName ? newLabelClassName :
+                        `absolute text-sm text-body duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-left inset-start-2.5 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 
+                        peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 
+                        rtl:peer-focus:left-auto 
                     ${labelClassName}
                     `}`}
                 >
